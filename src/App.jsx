7721f5d443f6e0cd71bc147d51b01d364d1950cb
David <j.david.prado1716@gmail.com>
2022-10-10 20:24:57 -0500

    Creando el contenedor y el text field

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import React from "react"
+import {Container} from "@mui/material"
+import Searcher from "./components/Searcher/Searcher"
 
 function App() {
 
-
   return (
-    <div>
-      holiiiii
-    </div>
+    <Container sx={{
+      background: 'whitesmoke',
+      width: '80vh',
+      height: '500 px',
+      borderRadius: '16px',
+      marginTop: '40px',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    }}>
+      <Searcher/>
+    </Container>
   )
 }
 
---
+++ src/components/Searcher/Searcher.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { IconButton, Stack, TextField } from '@mui/material'
+
+function Searcher() {
+    return (
+    <Stack>
+        <TextField>
+        </TextField>
+        <IconButton>
+        </IconButton>
+    </Stack>
+    )
+}
+
+export default Searcher
