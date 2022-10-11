c88b1b9f49cd07ad72babd6548a3103a6b92f444
David <j.david.prado1716@gmail.com>
2022-10-10 21:41:10 -0500

    Colocando los estilos del buscador

--- src/components/Searcher/Searcher.jsx
+++ src/components/Searcher/Searcher.jsx
@@ -1,12 +1,39 @@
 import React from 'react'
 import { IconButton, Stack, TextField } from '@mui/material'
+import SearchIcon from '@mui/icons-material/Search';
 
 function Searcher() {
+    const handleSubmit = () =>{
+
+    }
+    
     return (
-    <Stack>
-        <TextField>
+    <Stack 
+        direction = 'row'
+        sx ={{
+            marginTop: '30px',
+            width: '80%'
+    }}>
+        <TextField
+            id="outlined-basic" 
+            label="Github User"
+            placeholder='Buscar usuario Github'
+            variant="outlined"
+            size = 'small'
+            sx={{
+                width: '90%',
+                margin: "0 auto",
+            }}
+        >
         </TextField>
-        <IconButton>
+
+        <IconButton 
+            onClick={handleSubmit}
+            size= 'small'
+            sx= {{
+                left: "-45px"
+            }}>
+            <SearchIcon/>
         </IconButton>
     </Stack>
     )
